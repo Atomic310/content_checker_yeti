9d435363cbf8f8abefe3733a7432347a453cf509
atomic310 <atomic310@tamu.edu>
2022-04-11 09:51:16 -0500

    Slider value change stays on reload

--- slider.js
+++ slider.js
@@ -1,8 +1,17 @@
-function slidefunction(){
+function slidefunction(value){
   console.log('slider changed');
   const fontsize = document.querySelector('style');
-  var value = document.getElementById("textrange").value;
+  //var value = document.getElementById("textrange").value;
   console.log(value);
   fontsize.innerHTML = 'head {font-size: '+ value +'px;}';
   fontsize.innerHTML = 'body {font-size: '+ value +'px;}';
+  localStorage.setItem("fontvaluestore",value);
+}
+
+window.onload = function(){
+	if(localStorage.getItem("fontvaluestore").length>1){
+		var valueload = localStorage.getItem("fontvaluestore");
+    document.getElementById("textrange").value = valueload;
+    slidefunction(valueload);
+	}
 }
