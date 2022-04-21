42f13a28a360138ee8d25539e3abcc89e9028bdb
atomic310 <atomic310@tamu.edu>
2022-04-21 10:07:23 -0500

    regex to check if url or ip

--- url.js
+++ url.js
@@ -1,20 +1,43 @@
 function urlfunction(){
 	var urlinput = document.getElementById("ip").value;
-	console.log('{ "ip": "'+urlinput+'" }');
-	fetch('https://yeti.roe.hr/api/v1/check/url', {
-		method: 'post',
-		body: '{ "url": "'+urlinput+'" }'
-	})
-	.then(response => response.json())
-	.then(data =>{
-		if(data.resp == true){
-			document.getElementById("res").value = "Found a yeti!\n"
-			document.getElementById("res").value += "Link is potentially malicious"
-		}
-		else{
-			document.getElementById("res").value = "No yetis spotted!\n"
-			document.getElementById("res").value += "No malicious content found"
-		}
-	})
+	//console.log('{ "ip": "'+urlinput+'" }');
+	//regex determine if ip/url
+	var type = /[a-zA-Z]/;
+	//console.log('{ "type": "'+type+'" }');
+	//console.log('{ "test": "'+type.test(urlinput)+'" }');
+	if(type.test(urlinput) == true){
+		fetch('https://yeti.roe.hr/api/v1/check/url', {
+			method: 'post',
+			body: '{ "url": "'+urlinput+'" }'
+		})
+		.then(response => response.json())
+		.then(data =>{
+			if(data.resp == true){
+				document.getElementById("res").value = "Found a yeti!\n"
+				document.getElementById("res").value += "Link is potentially malicious"
+			}
+			else{
+				document.getElementById("res").value = "No yetis spotted!\n"
+				document.getElementById("res").value += "No malicious content found in link"
+			}
+		})
+	}
+	else{
+		fetch('https://yeti.roe.hr/api/v1/check/ip', {
+			method: 'post',
+			body: '{ "ip": "'+urlinput+'" }'
+		})
+		.then(response => response.json())
+		.then(data =>{
+			if(data.resp == true){
+				document.getElementById("res").value = "Found a yeti!\n"
+				document.getElementById("res").value += "IP is potentially malicious"
+			}
+			else{
+				document.getElementById("res").value = "No yetis spotted!\n"
+				document.getElementById("res").value += "No malicious content found in IP"
+			}
+		})
+	}
 
 }
