4b6ae0a433c0b3b366b6549c21565349269cec19
atomic310 <atomic310@tamu.edu>
2022-05-01 18:22:48 -0500

    file msg

--- file.js
+++ file.js
@@ -9,14 +9,12 @@ function filefunction() {
 		.then(response => response.json())
 		.then(data =>{
 			if(data.resp.detections != 0){
-				document.getElementById("res").value = "Found a yeti!\n"
-				document.getElementById("res").value += "Link is potentially malicious\n"
+				document.getElementById("res").value = "Link is potentially malicious\n"
         document.getElementById("res").value += "Name of malware is "+data.resp.name+"\n";
         document.getElementById("res").value += "It is a "+data.resp.category;
 			}
 			else{
-				document.getElementById("res").value = "No yetis spotted!\n"
-				document.getElementById("res").value += "No malicious content found\n"
+				document.getElementById("res").value = "No malicious content found\n"
         //console.log('{ "detections": "'+data.resp.detections+'" }');
 			}
 		})
